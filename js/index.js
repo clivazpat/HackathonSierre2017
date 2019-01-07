151c8096f13d4ffc0003bda1bb5da0503020db87
Patrick Clivaz <pat.clivaz@icloud.com>
2019-01-07 19:28:42 +0100

    Code added

---
+++ js/index.js
@@ -0,0 +1,152 @@
+jQuery(document).ready(function($) {
+    $("#star1").click(function(event) {
+        if (event.target == document.getElementById('star1')) {
+            $("#star1").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star2").click(function(event) {
+        if (event.target == document.getElementById('star2')) {
+            $("#star1").attr('src', 'images/pleine_Petit.jpg');
+            $("#star2").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star3").click(function(event) {
+        if (event.target == document.getElementById('star3')) {
+            $("#star1").attr('src', 'images/pleine_Petit.jpg');
+            $("#star2").attr('src', 'images/pleine_Petit.jpg');
+            $("#star3").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star4").click(function(event) {
+        if (event.target == document.getElementById('star4')) {
+            $("#star1").attr('src', 'images/pleine_Petit.jpg');
+            $("#star2").attr('src', 'images/pleine_Petit.jpg');
+            $("#star3").attr('src', 'images/pleine_Petit.jpg');
+            $("#star4").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star5").click(function(event) {
+        if (event.target == document.getElementById('star5')) {
+            $("#star1").attr('src', 'images/pleine_Petit.jpg');
+            $("#star2").attr('src', 'images/pleine_Petit.jpg');
+            $("#star3").attr('src', 'images/pleine_Petit.jpg');
+            $("#star4").attr('src', 'images/pleine_Petit.jpg');
+            $("#star5").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star6").click(function(event) {
+        if (event.target == document.getElementById('star6')) {
+            $("#star6").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star7").click(function(event) {
+        if (event.target == document.getElementById('star7')) {
+            $("#star6").attr('src', 'images/pleine_Petit.jpg');
+            $("#star7").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star8").click(function(event) {
+        if (event.target == document.getElementById('star8')) {
+            $("#star6").attr('src', 'images/pleine_Petit.jpg');
+            $("#star7").attr('src', 'images/pleine_Petit.jpg');
+            $("#star8").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star9").click(function(event) {
+        if (event.target == document.getElementById('star9')) {
+            $("#star6").attr('src', 'images/pleine_Petit.jpg');
+            $("#star7").attr('src', 'images/pleine_Petit.jpg');
+            $("#star8").attr('src', 'images/pleine_Petit.jpg');
+            $("#star9").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star10").click(function(event) {
+        if (event.target == document.getElementById('star10')) {
+            $("#star6").attr('src', 'images/pleine_Petit.jpg');
+            $("#star7").attr('src', 'images/pleine_Petit.jpg');
+            $("#star8").attr('src', 'images/pleine_Petit.jpg');
+            $("#star9").attr('src', 'images/pleine_Petit.jpg');
+            $("#star10").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star11").click(function(event) {
+        if (event.target == document.getElementById('star11')) {
+            $("#star11").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star12").click(function(event) {
+        if (event.target == document.getElementById('star12')) {
+            $("#star11").attr('src', 'images/pleine_Petit.jpg');
+            $("#star12").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star13").click(function(event) {
+        if (event.target == document.getElementById('star13')) {
+            $("#star11").attr('src', 'images/pleine_Petit.jpg');
+            $("#star12").attr('src', 'images/pleine_Petit.jpg');
+            $("#star13").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star14").click(function(event) {
+        if (event.target == document.getElementById('star14')) {
+            $("#star11").attr('src', 'images/pleine_Petit.jpg');
+            $("#star12").attr('src', 'images/pleine_Petit.jpg');
+            $("#star13").attr('src', 'images/pleine_Petit.jpg');
+            $("#star14").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star15").click(function(event) {
+        if (event.target == document.getElementById('star15')) {
+            $("#star11").attr('src', 'images/pleine_Petit.jpg');
+            $("#star12").attr('src', 'images/pleine_Petit.jpg');
+            $("#star13").attr('src', 'images/pleine_Petit.jpg');
+            $("#star14").attr('src', 'images/pleine_Petit.jpg');
+            $("#star15").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star16").click(function(event) {
+        if (event.target == document.getElementById('star16')) {
+            $("#star16").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star17").click(function(event) {
+        if (event.target == document.getElementById('star17')) {
+            $("#star16").attr('src', 'images/pleine_Petit.jpg');
+            $("#star17").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star18").click(function(event) {
+        if (event.target == document.getElementById('star18')) {
+            $("#star16").attr('src', 'images/pleine_Petit.jpg');
+            $("#star17").attr('src', 'images/pleine_Petit.jpg');
+            $("#star18").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star19").click(function(event) {
+        if (event.target == document.getElementById('star19')) {
+            $("#star16").attr('src', 'images/pleine_Petit.jpg');
+            $("#star17").attr('src', 'images/pleine_Petit.jpg');
+            $("#star18").attr('src', 'images/pleine_Petit.jpg');
+            $("#star19").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+    $("#star20").click(function(event) {
+        if (event.target == document.getElementById('star20')) {
+            $("#star16").attr('src', 'images/pleine_Petit.jpg');
+            $("#star17").attr('src', 'images/pleine_Petit.jpg');
+            $("#star18").attr('src', 'images/pleine_Petit.jpg');
+            $("#star19").attr('src', 'images/pleine_Petit.jpg');
+            $("#star20").attr('src', 'images/pleine_Petit.jpg');
+        }
+    })
+
+    $("#bleu").click(function(event) {
+        var x = document.getElementById('idefix');
+        if (x.style.display == 'block') {
+            x.style.display = 'none';
+        } else {
+            x.style.display = 'block';
+        }
+    });
+
+});
